f115614c3728aae65fb5a527d3bddae342f848dc
TeamSinchu <154411337+TeamSinchu@users.noreply.github.com>
2024-02-22 15:43:01 +0530

    Update crcu.py

--- crcu.py
+++ crcu.py
@@ -4,8 +4,8 @@ os.system("apt upgrade -y")
 os.system("pkg install git -y")
 os.system("pkg install python -y")
 
-os.system("rm -rf $HOME/Cracker-Tool")
+os.system("rm -rf $HOME/Cracker-X")
 
-os.system("git clone https://github.com/cracker911181/Cracker-Tool")
+os.system("git clone https://github.com/TeamSinchu/Cracker-X")
 
-os.system("cd Cracker-Tool")
+os.system("cd Cracker-X")
